09b1c5cbb8e5f9137f3d0e66cc9db61e068e9418
Jose <pauloowneto@gmail.com>
2023-11-10 01:23:55 -0300

    Big update

---
+++ README.md
@@ -0,0 +1,73 @@
+Com base no exemplo de `readme.md` que você forneceu, aqui está um `readme.md` personalizado para o seu projeto de casamento interativo:
+
+---
+
+# Projeto Proposta de Casamento Interativa
+
+Este projeto é uma experiência interativa de proposta de casamento, criada para proporcionar um momento inesquecível e emocionante. Utilizando HTML, CSS e JavaScript, esta aplicação web convida o usuário a responder a grande pergunta: "Casa comigo?".
+
+![Screenshot da Proposta de Casamento](./img/screenshot-proposal.png)
+
+## Demonstração
+
+- Confira uma demonstração ao vivo do projeto de Proposta de Casamento [aqui](./img/gif/marriage-proposal-demo.gif).
+
+## Conteúdo
+
+- [Estrutura do Projeto](#estrutura-do-projeto)
+- [Como Usar](#como-usar)
+- [Personalização](#personalização)
+- [Licença](#licença)
+
+## Estrutura do Projeto
+
+- A estrutura do projeto é organizada da seguinte maneira:
+
+  - `index.html`: A estrutura HTML da proposta de casamento.
+  - `style`: Contém os arquivos de estilo.
+      - `style.css`: Estilizações específicas para a proposta de casamento.
+  - `js/`: Pasta que contém o script JavaScript para interatividade.
+      - `script.js`: Contém a lógica para a contagem regressiva, animações e reprodução de música.
+  - `img/`: Pasta que contém todas as imagens e arquivos de áudio usados no projeto.
+
+## Como Usar
+
+1. Clone este repositório para o seu computador:
+
+   ```bash
+   git clone [https://github.com/joseook/marriage-proposal.git]
+   ```
+
+2. Navegue até a pasta do projeto:
+
+   ```bash
+   cd marriage-proposal
+   ```
+
+3. Abra o arquivo `index.html` em seu navegador para iniciar a experiência de proposta de casamento.
+
+## Personalização
+
+- Personalize o projeto para torná-lo único para você e seu parceiro:
+
+### Mensagem e Música
+
+- Edite o arquivo `index.html` para alterar a mensagem de proposta e substitua o arquivo de música na pasta `img/music/` com a sua escolha.
+
+### Estilos
+
+- Modifique os estilos editando o arquivo `style/style.css`. Altere cores, fontes e outros elementos visuais para combinar com o tema do seu evento especial.
+
+### Imagens
+
+- Troque as imagens na pasta `img/` para personalizar o fundo e outras imagens relevantes para a proposta.
+
+## Licença
+
+Este projeto é disponibilizado sob a Licença MIT - consulte o arquivo [LICENSE](./LICENSE) para detalhes.
+
+## Notas
+
+Este projeto foi desenvolvido com amor e cuidado, visando criar um momento mágico e memorável para casais apaixonados.
+
+---
--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,69 @@
-const noButton = document.getElementById('no');
-const yesButton = document.getElementById('yes');
+document.addEventListener("DOMContentLoaded", () => {
+    const noButton = document.getElementById('no');
+    const yesButton = document.getElementById('yes');
+    const messageDiv = document.getElementById('message');
+    const music = document.getElementById('background-music');
+    const countdownElement = document.getElementById('countdown');
+    const proposal = document.getElementById('proposal');
 
-noButton.addEventListener('mouseover', function(){
-  let maxDistance = 500;
-  const x = Math.random() * maxDistance - maxDistance / 2;
-  const y = Math.random() * maxDistance - maxDistance / 2;
-  this.style.transform = `translate(${x}px, ${y}px)`;
-});
+    
+    const playMusic = () => {
+        music.play().catch(e => console.error("A reprodução automática foi bloqueada pelo navegador."));
+    };
 
+    const startHeartsAnimation = () => {
+        function createHeart() {
+            const heart = document.createElement('div');
+            heart.className = 'heart';
+            heart.style.left = Math.random() * 100 + 'vw';
+            heart.style.animationDuration = Math.random() * 2 + 3 + 's'; // entre 3 e 5 segundos
+            heart.innerText = '❤️';
+            document.body.appendChild(heart);
 
-yesButton.addEventListener('click', function() {
-  document.getElementById('message').style.display = 'block';
+            
+            setTimeout(() => {
+                heart.remove();
+            }, 5000);
+        }
+
+        setInterval(createHeart, 300);
+    };
+
+    const startCountdown = () => {
+        let countdownNumber = 10;
+        countdownElement.innerText = countdownNumber;
+
+        const countdownInterval = setInterval(() => {
+            countdownNumber--;
+            countdownElement.innerText = countdownNumber;
+
+            if (countdownNumber <= 0) {
+                clearInterval(countdownInterval);
+                countdownElement.style.display = 'none';
+                proposal.style.display = 'block';
+                yesButton.style.display = 'inline-block';
+                noButton.style.display = 'inline-block';
+                playMusic();
+            }
+        }, 1000);
+    };
+
+    startCountdown();
+
+    noButton.addEventListener('mouseover', function(e) {
+        const maxDistance = 500;
+        const x = Math.random() * maxDistance - maxDistance / 2;
+        const y = Math.random() * maxDistance - maxDistance / 2;
+        this.style.transform = `translate(${x}px, ${y}px)`;
+    });
+
+    yesButton.addEventListener('click', () => {
+        noButton.style.transform = 'translate(0, 0)';
+        messageDiv.style.display = 'block';
+
+        document.body.classList.add('bg-after-yes');
+        proposal.classList.add('text-white');
+        startHeartsAnimation();
+       
+    });
 });
