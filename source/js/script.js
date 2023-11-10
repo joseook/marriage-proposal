fe923fe93cb52f78c3441afcf64715208998f7e1
Jose <pauloowneto@gmail.com>
2023-11-09 21:07:04 -0300

    Upgrade all

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,14 @@
 const noButton = document.getElementById('no');
-noButton.addEventListener('mouseover', function() {
+const yesButton = document.getElementById('yes');
+
+noButton.addEventListener('mouseover', function(){
   let maxDistance = 500;
   const x = Math.random() * maxDistance - maxDistance / 2;
   const y = Math.random() * maxDistance - maxDistance / 2;
   this.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-const yesButton = document.getElementById('yes');
+
 yesButton.addEventListener('click', function() {
   document.getElementById('message').style.display = 'block';
 });
